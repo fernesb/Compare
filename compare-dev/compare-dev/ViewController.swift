1807fdd7a1410ba67cc524c9fc90772d0355e0fc
Shuyang Yu <yu2@cooper.edu>
2017-05-16 12:08:36 -0400

    setup IOS front end and node backend

---
+++ compare-dev/compare-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  compare-dev
+//
+//  Created by 于舒洋 on 5/16/17.
+//  Copyright © 2017 Compare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
