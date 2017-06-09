bf688fe6e7948b6fbfff9c8213adb4fa83d59945
Shuyang Yu <yu2@cooper.edu>
2017-06-09 12:16:22 -0400

    add search bar

--- compareapp/iosApp/Lobby/Contacts/ContactsPage.js
+++ compareapp/iosApp/Lobby/Contacts/ContactsPage.js
@@ -15,81 +15,50 @@ import {
 
 import { GiftedChat } from 'react-native-gifted-chat';
 import Row from './Row'
+import Search from 'react-native-search-box';
 
 export default class ContactsPage extends React.Component{
 
-  constructor(props) {
-    super(props);
-    
-    FlatList.pageSize = 100;
-    this.state = {messages: []};
-    // this.onSend = this.onSend.bind(this);
-  }
-  // componentWillMount() {
-  //   this.setState({
-  //     messages: [
-  //       {
-  //         _id: 1,
-  //         text: 'Hello developer',
-  //         createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-  //         user: {
-  //           _id: 2,
-  //           name: 'React Native',
-  //           avatar: 'https://facebook.github.io/react/img/logo_og.png',
-  //         },
-  //       },
-  //     ],
-  //   });
-  // }
-  // onSend(messages = []) {
-  //   this.setState((previousState) => {
-  //     return {
-  //       messages: GiftedChat.append(previousState.messages, messages),
-  //     };
-  //   });
-  // }
-  // render() {
-  //   return (
-  //     <GiftedChat
-  //       messages={this.state.messages}
-  //       onSend={this.onSend}
-  //       user={{
-  //         _id: 1,
-  //       }} />
-  //   );
-  // }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-            {key: 'Juliee'},
-            {key: 'Julieee'},
-            {key: 'Julieeee'},
-            {key: 'Juli'},
-            {key: 'Jule'},
-            {key: 'Juie'},
-            {key: 'Jlie'},
-            {key: 'ulie'},
-            {key: 'aJulie'},
-            {key: 'bJulie'},
-            {key: 'cJulie'},
-            {key: 'vJulie'},
-            {key: 'bcJulie'},
-            {key: 'nJulie'},
-            {key: 'mJulie'},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-          removeClippedSubviews = {false}/>
-      </View>
+     
+        <View style={{ flex: 1 }}>
+          <Search/>
+          <FlatList
+            data={[
+              {key: 'Devin'},
+              {key: 'Jackson'},
+              {key: 'James'},
+              {key: 'Joel'},
+              {key: 'John'},
+              {key: 'Jillian'},
+              {key: 'Jimmy'},
+              {key: 'Julie'},
+              {key: 'Juliee'},
+              {key: 'Julieee'},
+              {key: 'Julieeee'},
+              {key: 'Juli'},
+              {key: 'Jule'},
+              {key: 'Juie'},
+              {key: 'Jlie'},
+              {key: 'ulie'},
+              {key: 'aJulie'},
+              {key: 'bJulie'},
+              {key: 'cJulie'},
+              {key: 'vJulie'},
+              {key: 'bcJulie'},
+              {key: 'nJulie'},
+              {key: 'mJulie'},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+            removeClippedSubviews = {false}/>
+        </View>
+
+        // <View style={styles.container}>
+        //   
+        // </View>
+      
     );
   }
 
